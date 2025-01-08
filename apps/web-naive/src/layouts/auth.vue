ebc3eab457fa6bdb9e29a56371e5ef5f34b66203
su882 <soowqx@gmail.com>
2025-01-08 17:37:53 +0800

    Initial commit

---
+++ apps/web-naive/src/layouts/auth.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { AuthPageLayout } from '@vben/layouts';
+import { preferences } from '@vben/preferences';
+
+const appName = computed(() => preferences.app.name);
+const logo = computed(() => preferences.logo.source);
+</script>
+
+<template>
+  <AuthPageLayout :app-name="appName" :logo="logo">
+    <!-- 自定义工具栏 -->
+    <!-- <template #toolbar></template> -->
+  </AuthPageLayout>
+</template>
---
+++ packages/effects/common-ui/src/ui/authentication/third-party-login.vue
@@ -0,0 +1,29 @@
+<!--第三方登录-->
+<script setup lang="ts">
+import { SvgdingdingIcon } from '@vben/icons';
+import { $t } from '@vben/locales';
+
+import { VbenIconButton } from '@vben-core/shadcn-ui';
+
+defineOptions({
+  name: 'ThirdPartyLogin',
+});
+</script>
+
+<template>
+  <div class="w-full sm:mx-auto md:max-w-md">
+    <div class="mt-4 flex items-center justify-between">
+      <span class="border-input w-[35%] border-b dark:border-gray-600"></span>
+      <span class="text-muted-foreground text-center text-xs uppercase">
+        {{ $t('authentication.thirdPartyLogin') }}
+      </span>
+      <span class="border-input w-[35%] border-b dark:border-gray-600"></span>
+    </div>
+
+    <div class="mt-4 flex flex-wrap justify-center">
+      <VbenIconButton class="mb-3">
+        <SvgdingdingIcon class="size-600" />
+      </VbenIconButton>
+    </div>
+  </div>
+</template>
